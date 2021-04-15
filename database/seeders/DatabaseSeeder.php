30475037f1fd71825329f702b277d6aba01983ad
Edward Guevara <edwardgvr414@gmail.com>
2021-04-15 20:38:11 +0000

    Seeded zonas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(usersSeeder::class);
+        $this->call(ZonasSeeder::class);
     }
 }
--- database/seeders/ZonasSeeder.php
+++ database/seeders/ZonasSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database;
+use Illuminate\Support\Facades\DB;
 
 class ZonasSeeder extends Seeder
 {
@@ -13,6 +15,19 @@ class ZonasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('zonas')->insert([
+            [
+                "zona" => "Occidental"
+            ],
+            [
+                "zona" => "Central"
+            ],
+            [
+                "zona" => "Paracentral"
+            ],
+            [
+                "zona" => "Oriental"
+            ]
+        ]);
     }
 }
