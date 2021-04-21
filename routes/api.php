93465dfcfd8563667c6f2562adc60a913affac27
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2021-04-21 12:35:54 -0600

    Changed passport table provide to Usuarios

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get("/departamentos/byzona/{id}", "DepartamentosController@getDptosByZona");
+});
+
 Route::post("/login", "UserController@login");
 
 Route::ApiResource("/usuarios", "UserController");
@@ -27,7 +31,7 @@ Route::ApiResource("/posts", "PostController");
 Route::ApiResource("/zonas", "ZonasController");
 
 Route::ApiResource("/departamentos", "DepartamentosController");
-Route::get("/departamentos/byzona/{id}", "DepartamentosController@getDptosByZona");
+// Route::get("/departamentos/byzona/{id}", "DepartamentosController@getDptosByZona");
 
 Route::ApiResource("/municipios", "MunicipiosController");
 Route::get("municipios/bydpto/{id}", "MunicipiosController@showByDpto");
